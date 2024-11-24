93fadd2b9f022074215a942726e94529f6dbc008
0xfzyy <122592126+0xfzyy@users.noreply.github.com>
2024-11-24 12:53:07 +0800

    script.js

--- script.js
+++ script.js
@@ -20,6 +20,6 @@ document.addEventListener('DOMContentLoaded', () => {
     // 背景颜色切换
     document.body.addEventListener('click', () => {
         document.body.style.backgroundColor = 
-            document.body.style.backgroundColor === 'rgb(244, 244, 249)' ? '#e0f7fa' : '#f4f4f9';
+            document.body.style.backgroundColor === 'rgb(240, 248, 255)' ? '#e0f7fa' : '#f0f8ff';
     });
 });
